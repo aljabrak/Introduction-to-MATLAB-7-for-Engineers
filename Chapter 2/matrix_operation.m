b4366808c9c6c9952cde852689e55e19b4f78cb1
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-09-19 23:45:16 +0500

    Create matrix_operation.m

---
+++ Chapter 2/matrix_operation.m
@@ -0,0 +1,42 @@
+%Matrix Operation.
+
+%Scalar Addition.
+C = A + 2;
+disp(C);
+
+%Scalar Subtraction.
+C = B - 2;
+disp(C)
+
+%Scalar Multiplication.
+A = [2, 3; -7, 8];
+disp(3*A);
+
+%Scalar Division.
+A = [2, 3; -7, 8];
+disp(A/3);
+
+%Matrix Addition.
+B = [3, 5; 9, 2];
+C = A+B;
+disp(C);
+
+%Matrix Subtraction.
+C = A-B;
+disp(C);
+
+%Matrix Multiplication.
+C = A*B;
+disp(C);
+
+%Matrix  Right Division.
+C = A./B;
+disp(C);
+
+%Matrix Left Division.
+C = A.\B;
+disp(C);
+
+%Matrix Exponent.
+C = A.^B;
+disp(C);
