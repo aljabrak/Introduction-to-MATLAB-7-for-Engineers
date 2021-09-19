2a3cb639dfb728eff1fd23d66f0412e7d4308192
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-09-19 23:52:56 +0500

    Create vector_operations.m

---
+++ Chapter 2/vector_operations.m
@@ -0,0 +1,41 @@
+% Vector Operation.
+
+% Scalar Addition.
+a = [2, 3];
+c = a + 2;
+disp(c);
+
+% Scalar Subtraction.
+c = a - 2;
+disp(c);
+
+% Scalar Multiplication.
+disp(3*a);
+
+% Scalar Division.
+disp(a/3);
+
+% Vector Addition.
+b = [9, 2];
+c = a + b;
+disp(c);
+
+% Vector Subtraction.
+c = a - b;
+disp(c);
+
+% Vector Multiplication.
+c = a*b;
+disp(c);
+
+% Vector  Right Division.
+c = a./b;
+disp(c);
+
+% Vector Left Division.
+c = a.\b;
+disp(c);
+
+% Vector Exponent.
+c = a.^b;
+disp(c);
